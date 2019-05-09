a768efc5b0774ef35bf2b089c3b1571cb687e5f9
“wzy <1757629766@qq.com>
2019-05-09 18:44:33 +0800

    安装并简单应用了vuex

--- src/main.js
+++ src/main.js
@@ -2,8 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import {router} from './router/index';
 
-const app = new Vue({
+// 如果在模块化构建系统中，请确保在开头调用了 Vue.use(Vuex)
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state, payload) {
+      state.count += payload.num;
+    }
+  }
+})
+
+new Vue({
   el: '#app',
   router: router,
+  store,
   render: h => h(App)
 })
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,30 @@
 <template>
   <div>
     <p>Welcome home</p>
+    <hr />
+    vuex实现计算：{{count}}
+    <button @click="add">add</button>
   </div>
 </template>
 
 <script>
  
   export default {
+    computed: {
+      count () {
+        return this.$store.state.count
+      }
+    },
     components: {
       
+    },
+    methods: {
+      add (){
+        this.$store.commit('increment',{
+          num: 10
+        })
+      }
     }
+   
   }
 </script>
